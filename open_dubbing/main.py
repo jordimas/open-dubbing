96c25c7df061ec389f4cb30e5b7d08f28ab9e07c
Jordi Mas <jmas@softcatala.org>
2024-09-30 21:53:47 +0200

    Fix name in source code headers

--- open_dubbing/main.py
+++ open_dubbing/main.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Jordi Mas i Herǹandez <jmas@softcatala.org>
+# Copyright 2024 Jordi Mas i Hernàndez <jmas@softcatala.org>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
