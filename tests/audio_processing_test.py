cdc1ca13d6246946885626c86e59b4ed7959460b
Jordi Mas <jmas@softcatala.org>
2024-10-20 15:58:46 +0200

    Fix

--- tests/audio_processing_test.py
+++ tests/audio_processing_test.py
@@ -46,9 +46,7 @@ class TestCreatePyannoteTimestamps:
                 audio_file=temporary_file.name,
                 pipeline=mock_pipeline,
             )
-            assert timestamps == {
-                "utterances": [{"start": 0.0, "end": 10, "speaker_id": "SPEAKER_00"}]
-            }
+            assert timestamps == [{"start": 0.0, "end": 10, "speaker_id": "SPEAKER_00"}]
 
 
 class TestCutAndSaveAudio:
